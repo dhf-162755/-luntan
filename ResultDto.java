c83717d37118960e17e02655e95b649daa7fa6d9
dhf-162755 <75711858+dhf-162755@users.noreply.github.com>
2021-11-21 13:20:21 +0800

    Add files via upload

---
+++ ResultDto.java
@@ -0,0 +1,55 @@
+package com.sdxb.blog.dto;
+
+
+public class ResultDto<T> {
+    private int code;
+    private String message;
+    private T data;
+
+    public ResultDto() {
+    }
+
+    public ResultDto(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public ResultDto success(){
+        ResultDto resultDto=new ResultDto();
+        resultDto.setCode(200);
+        resultDto.setMessage("成功");
+        return resultDto;
+    }
+    public <T> ResultDto success(T data){
+        ResultDto resultDto=new ResultDto();
+        resultDto.setCode(200);
+        resultDto.setMessage("成功");
+        resultDto.setData(data);
+        return resultDto;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
